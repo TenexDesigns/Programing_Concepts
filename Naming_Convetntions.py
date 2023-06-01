f7aa3c48b2bd92c303fb975ac05ac1463151b45e
TenexDesigns <117345133+TenexDesigns@users.noreply.github.com>
2023-06-01 19:39:51 +0300

    Create Naming_Convetntions.py

---
+++ Naming_Convetntions.py
@@ -0,0 +1,45 @@
+Naming conventions, such as PascalCase, camelCase, and snake_case, are used to define consistent 
+and readable naming patterns for variables, functions, classes, and other identifiers in programming languages. 
+Each convention has its own rules and is commonly used in different programming languages or communities. 
+Lets explore some of the most widely used naming conventions:
+
+  
+  
+PascalCase (or UpperCamelCase):
+
+Rules: The first letter of each word is capitalized, and there are no separators between the words.
+Example: MyClass, EmployeeRecord, CalculateSalary
+PascalCase is commonly used in languages like C#, Java, and Ruby for class names and other type declarations.
+
+
+
+
+
+camelCase (or lowerCamelCase):
+
+Rules: The first letter of the first word is lowercase, and the first letter of subsequent words is capitalized. 
+  There are no separators between the words.
+Example: myVariable, calculateSalary, numberOfStudents
+camelCase is frequently used in languages like JavaScript, Python, and Ruby for variable and function names.
+
+snake_case:
+
+Rules: Words are written in lowercase, and words are separated by underscores.
+Example: my_variable, calculate_salary, number_of_students
+snake_case is popular in languages like Ruby, Python, and Rust for variable names and function names.
+
+kebab-case (or spinal-case):
+
+Rules: Words are written in lowercase, and words are separated by hyphens.
+Example: my-variable, calculate-salary, number-of-students
+kebab-case is often used in CSS, HTML, and URL slugs, but less common in programming language identifiers.
+
+SCREAMING_SNAKE_CASE:
+
+Rules: Words are written in uppercase, and words are separated by underscores.
+Example: MY_CONSTANT, MAX_VALUE, CONFIG_FILE_NAME
+SCREAMING_SNAKE_CASE is typically used for constants or global variables in languages like Ruby and Python.
+
+Its important to note that the choice of naming convention is often determined by the programming language or 
+the style guide of a particular project or community. 
+Consistency and readability are key considerations when choosing a naming convention, as it helps make your code more understandable and maintainable.
